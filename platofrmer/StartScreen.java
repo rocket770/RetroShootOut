677395449fa26404231537de7549c80908d8d324
rocket770 <skrillx789@hotmail.com>
2020-07-29 09:53:07 +1000

    test push

--- platofrmer/StartScreen.java
+++ platofrmer/StartScreen.java
@@ -18,17 +18,10 @@ public class StartScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1000, 700, 1); 
         Greenfoot.start();
-        prepare();
-    }
-
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
-    private void prepare()
-    {
         addObject(new HelpButton(),500,525);
         addObject(new StartButton(),500,350);
         addObject(new SelectEnemy(),89,340);
     }
+
+
 }
